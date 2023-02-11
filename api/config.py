d0ee9d35044391504d401eca109aaaa95e426014
brayohmurithidev <murithibrianm@gmail.com>
2023-02-11 23:21:06 +0300

    database config settings and app initialization

---
+++ api/config.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/job_prospects'
+app.config['SQLALCHEMY_DATABASE_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
