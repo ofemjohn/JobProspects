ad8b332386a69281b7fba0664e1b5b8aab198aad
ofemjohn <ofemjohn@gmail.com>
2023-02-13 19:26:36 +0100

    added pymysql to the database config

--- api/config.py
+++ api/config.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/job_prospects'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:56465646@localhost/job_prospects'
 app.config['SQLALCHEMY_DATABASE_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
