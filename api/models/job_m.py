e0803e0d44c7e817ca9f2daced418ad99556fd38
brayohmurithidev <murithibrianm@gmail.com>
2023-02-16 00:48:37 +0300

    job model and route updates

--- api/models/job_m.py
+++ api/models/job_m.py
@@ -4,28 +4,28 @@ from config import db, app
 class Job(db.Model):
     job_id = db.Column(db.Integer, primary_key=True)
     job_title = db.Column(db.String(100), nullable=False)
-    employment_type = db.Column(db.String(50), nullable=False)
+    employment_type = db.Column(db.String(50))
     job_description = db.Column(db.Text, nullable=False)
     job_apply_link = db.Column(db.String(255), nullable=False)
-    employer_website = db.Column(db.String(100), nullable=False)
+    employer_website = db.Column(db.String(100))
     employer_name = db.Column(db.String(50), nullable=False)
-    employer_logo = db.Column(db.String(255), nullable=False)
+    employer_logo = db.Column(db.String(255))
     job_is_remote = db.Column(db.Boolean, nullable=False)
     job_posted_at_timestamp = db.Column(db.String(50))
-    job_posted_at_datetime_utc = db.Column(db.DateTime())
+    job_posted_at_datetime_utc = db.Column(db.String(100))
     job_city = db.Column(db.String(100))
     job_state = db.Column(db.String(100))
     job_country = db.Column(db.String(100))
     job_benefits = db.Column(db.Text)
-    job_google_link = db.Column(db.String(100))
-    job_offer_expiration_datetime_utc = db.Column(db.DateTime())
+    job_google_link = db.Column(db.String(512))
+    job_offer_expiration_datetime_utc = db.Column(db.String(100))
     job_required_experience = db.Column(db.PickleType)
     job_required_education = db.Column(db.PickleType)
     job_required_skills = db.Column(db.PickleType)
     job_highlights = db.Column(db.PickleType)
-    job_salary = db.Column(db.String(10))
-    job_salary_period = db.Column(db.String(10))
-    job_salary_currency = db.Column(db.String(10))
+    # job_salary = db.Column(db.String(10))
+    # job_salary_period = db.Column(db.String(10))
+    # job_salary_currency = db.Column(db.String(10))
 
     # TO STORE THE PICKLETYPE
     # data = {'key1': [1,3,4,6], 'key2': ['a', 'b', 'c]}
@@ -35,7 +35,7 @@ class Job(db.Model):
         return {
             "job_id": self.job_id,
             "job_title": self.job_title,
-            "employee__type": self.employment_type,
+            "employment_type": self.employment_type,
             "job_description": self.job_description,
             "job_apply_link": self.job_apply_link,
             "employer_website": self.employer_website,
@@ -54,9 +54,9 @@ class Job(db.Model):
             "job_required_education": self.job_required_education,
             "job_highlights": self.job_highlights,
             "job_required_skills": self.job_required_skills,
-            "job_salary": self.job_salary,
-            "job_salary_period": self.job_salary_period,
-            "job_salary_currency": self.job_salary_currency,
+            # "job_salary": self.job_salary,
+            # "job_salary_period": self.job_salary_period,
+            # "job_salary_currency": self.job_salary_currency,
         }
 
 
