0e39ebb581faa58c285395ef2b583def8dc18271
brayohmurithidev <murithibrianm@gmail.com>
2023-02-17 14:22:22 +0300

    created company auth rout

--- api/routes/auth.py
+++ api/routes/auth.py
@@ -1,8 +1,11 @@
 from models.user_m import User
+from models.companies_m import Companies
+from werkzeug.utils import secure_filename
 from flask import request, jsonify
 from config import db, app
 from flask_bcrypt import Bcrypt
 from datetime import datetime
+import os
 from flask_jwt_extended import JWTManager, create_access_token, set_access_cookies, unset_jwt_cookies
 import logging
 
@@ -82,6 +85,58 @@ def logout():
     response = jsonify({"msg": "logout successful"})
     unset_jwt_cookies(response)
     return response
+
+
+# COMPANIES AUTH
+@app.route("/companies/auth/register", methods=["POST"])
+def register_company():
+    company_data = request.form
+    company_name = company_data.get("company_name")
+    company_email = company_data.get("company_email")
+    company_country = company_data.get("company_country")
+    company_website = company_data.get("company_website")
+    password = company_data.get("password")
+    # file
+
+    # CHECK IF FIELDS NOT EMPTY
+    if not company_name or not company_email or not company_country or not company_website or 'file' not in request.files or not password:
+        return jsonify({"message": "Fields cannot be empty"}), 204
+    # Extract filename and check if its allowed
+    else:
+        componay_logo = request.files.get('file')
+        # Define allowed extensions
+        ALLOWED_EXTENSIONS = {'jpeg', 'jpg', 'png'}
+
+        # check if file meets the extension requirements
+
+        def allowed_file(filename):
+            return '.' in filename and \
+                filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+        filename = secure_filename(componay_logo.filename)
+        if not allowed_file(filename):
+            return jsonify({"Message": "File is not allowed, can oly be png, jpeg or jpg"}), 403
+        else:
+            if not os.path.exists('file_upload'):
+                os.makedirs('file_upload')
+            componay_logo.save(os.path.join('file_upload', filename))
+            logo_url = os.path.join("file_upload", filename)
+            if logo_url:
+                pwd_hash = bcrypt.generate_password_hash(
+                    password, 5).decode('utf-8')
+                newCompany = Companies(company_name=company_name, company_email=company_email,
+                                       company_country=company_country, company_website=company_website, company_logo_url=logo_url, password=pwd_hash)
+                try:
+                    db.session.add(newCompany)
+                    db.session.commit()
+                    return jsonify({"message": "Company Created Successfully"}), 200
+                except:
+                    db.session.rollback()
+                    existingCompany = Companies.query.filter(
+                        company_email == company_email).first()
+                    if existingCompany is not None:
+                        return {"message": f'Company with email: {company_email} already exists'}, 409
+                    else:
+                        return {"message": "An error occurred while creating the user"}, 500
 
 
 with app.app_context():
