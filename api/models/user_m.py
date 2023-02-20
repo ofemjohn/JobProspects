c1d396e565fe2aef426196ea0c2bed83019bb8b1
ofemjohn <ofemjohn@gmail.com>
2023-02-20 12:36:37 +0100

    minor fixes

--- api/models/user_m.py
+++ api/models/user_m.py
@@ -20,3 +20,5 @@ class User(db.Model):
     # RETURN A DICTONARY
     def to_dict(self):
         return {'user_id': self.user_id, 'first_name': self.first_name, 'last_name': self.last_name, 'email': self.email, 'phone': self.phone, 'gender': self.gender, 'country': self.country, 'state': self.state, 'address': self.address, 'date_created': self.date_created, 'last_login': self.last_login}
+
+
