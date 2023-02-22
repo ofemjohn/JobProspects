07ea8d7060ac1fbbcd2c1b86af4f978dc4ce05ce
brayohmurithidev <murithibrianm@gmail.com>
2023-02-23 00:35:34 +0300

    updated navbar

--- client/src/componets/navbar/Navbar.js
+++ client/src/componets/navbar/Navbar.js
@@ -1,30 +1,46 @@
 import { Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import ModalCustom from "../ModalCustom";
 import "./navbar.css";
 
-const Navbar = () => {
+const Navbar = ({ setOpen, setType }) => {
+  const handleOpenLogin = () => {
+    setOpen(true);
+    setType("login");
+  };
+  const handleOpenSignUp = () => {
+    setOpen(true);
+    setType("signup");
+  };
   return (
-    <div className="navbar">
-      <ul>
-        <li>
-          <Link className="navbar-link" to="/">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link className="navbar-link" to="/services">
-            Services
-          </Link>
-        </li>
-        <li>
-          <Button variant="outlined">Sign In</Button>
-        </li>
-        <li>
-          <Button variant="outlined">Sign Up</Button>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className="navbar">
+        <ul>
+          <li>
+            <Link className="navbar-link" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="navbar-link" to="/services">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Button onClick={handleOpenLogin} variant="outlined">
+              Hire
+            </Button>
+          </li>
+          <li>
+            <Button onClick={handleOpenSignUp} variant="outlined">
+              Looking for a Job?
+            </Button>
+          </li>
+        </ul>
+      </div>
+      {/* <ModalCustom show={show} setShow={setShow} /> */}
+    </>
   );
 };
 
