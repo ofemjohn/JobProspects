12d3b5c881772641fe83f0563faaabc1894ba837
brayohmurithidev <murithibrianm@gmail.com>
2023-02-23 21:35:33 +0300

    updates

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,56 +1,121 @@
-import { IconButton, Paper } from "@mui/material";
+import { Button, IconButton, Paper, Typography } from "@mui/material";
 import React from "react";
 
 import "./landing.css";
-import SearchIcon from "@mui/icons-material/Search";
-import Filter from "../componets/filter/Filter";
-import JobCard from "../componets/jobCard/JobCard";
 import { Box } from "@mui/system";
+import image from "../assets/images/hero-img.jpg";
+import { Link, redirect } from "react-router-dom";
+import CustomCarousel from "../componets/CustomCarousel";
+import { useNavigate } from "react-router-dom";
 
 const Landing = () => {
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    console.log("You clicked me");
+    navigate("/register_company");
+  };
+  const jobs = [
+    {
+      id: 1,
+      title: "React Developer",
+      description:
+        "We are looking for a self-motavaited and ready to learn and team play with react js experience to work together with our developer ...",
+    },
+    {
+      id: 2,
+      title: "Finance Officer",
+      description:
+        "We are looking for a self-motavaited and ready to learn and team play with react js experience to work together with our developer ...",
+    },
+    {
+      id: 3,
+      title: "Human Resource Intern",
+      description:
+        "We are looking for a self-motavaited and ready to learn and team play with react js experience to work together with our developer ...",
+    },
+    {
+      id: 4,
+      title: "Juniour Backend Developer",
+      description:
+        "We are looking for a self-motavaited and ready to learn and team play with react js experience to work together with our developer ...",
+    },
+  ];
   return (
-    <div>
-      {/* TITLE */}
-      <div className="landing-title">Find Your Dream Job Today</div>
-      {/* SEARCH INPUT */}
-      <div className="search-wrapper">
-        <input
-          className="search-input"
-          type="search"
-          placeholder="e.g Python Developer"
-        />
-        <IconButton>
-          <SearchIcon fontSize="large" />
-        </IconButton>
-      </div>
-      {/* FILTERS */}
-      <Box pb={5}>
-        <Filter />
-      </Box>
-      {/* JOBS */}
-      <Paper elevation={5}>
+    <>
+      {/* HERO SECTION */}
+      <Box
+        sx={{
+          height: "80vh",
+          display: "flex",
+        }}
+      >
+        {/* Left side */}
         <Box
-          p={3}
-          // display={{ x }}
           sx={{
-            display: "grid",
-            columnGap: 3,
-            rowGap: 1,
-            gridTemplateColumns: "repeat(4, 1fr)",
+            width: "60%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "20px",
+            justifyContent: "center",
+            padding: "20px 60px 20px 60px",
           }}
         >
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
+          <Typography variant="h3" sx={{ fontSize: "70px" }}>
+            Find Your Dream Job Today
+          </Typography>
+          <Typography variant="subtitle1" sx={{ fontSize: "24px" }}>
+            Looking for a job that matches your skills and experience? Look no
+            further! Our platform connects job seekers with the latest and
+            greatest job openings from top companies in your area. Whether
+            you're just starting out in your career or are a seasoned
+            professional, we've got the perfect job for you.
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              alignSelf: "flex-start",
+            }}
+          >
+            <Button
+              variant="contained"
+              size="large"
+              sx={{
+                backgroundColor: "#008080",
+                padding: "20px 30px",
+                borderRadius: "10px",
+              }}
+              onClick={handleNavigate}
+            >
+              Are You Hiring?
+            </Button>
+          </Box>
+          <Typography
+            sx={{
+              display: "flex",
+              alignSelf: "flex-start",
+              fontSize: "16px",
+            }}
+            variant="button"
+          >
+            {" "}
+            Looking for Jobs instead?
+            <Link style={{ color: "#008080" }} to="#">
+              Apply For Jobs{" "}
+            </Link>
+          </Typography>
         </Box>
-      </Paper>
-      {/* <div className="jobs">
-        
-      </div> */}
-    </div>
+        {/* Right side */}
+        <Box sx={{ width: "60%", marginTop: "20px" }}>
+          <img src={image} alt="hero bg" width="100%" />
+          <CustomCarousel jobs={jobs} />
+        </Box>
+      </Box>
+      {/* HERO TWO */}
+      <Box>
+        <Typography> </Typography>
+      </Box>
+    </>
   );
 };
 
