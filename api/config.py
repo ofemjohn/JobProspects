bb8877a3f7674b3af21b0a173da54d3dcb9603b7
brayohmurithidev <murithibrianm@gmail.com>
2023-03-09 15:12:44 +0300

    security updates

--- api/config.py
+++ api/config.py
@@ -5,16 +5,12 @@ import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/job_prospects'
-# os.environ.get('DATABASE_URL')
-# 'mysql://root:root@localhost/job_prospects'
-# mysql+pymysql://root:56465646@localhost/job_prospects
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_DATABASE_TRACK_MODIFICATIONS'] = False
 app.config['FLASK_DEBUG'] = True
-app.config["JWT_SECRET_KEY"] = "jobprospects"
+app.config["JWT_SECRET_KEY"] = os.environ.get('SECRET_KEY')
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
 app.config["JWT_COOKIE_SECURE"] = False
 app.config["JWT_TOKEN_LOCATION"] = ["headers"]
-app.config['CLOUDINARY_URL'] = 'cloudinary://273368127559497:Ypg4Y3dB94Edyc2DYXKYjqnlNAw@brayohmurithi'
-
+app.config['CLOUDINARY_URL'] = os.environ.get('CLOUDINARY_URL')
 db = SQLAlchemy(app)
